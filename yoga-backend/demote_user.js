55ad1d7ed40f0e2bede2cb154c6f4239032ea80f
samradh@123 <samradhmalli7@gmail.com>
2026-03-11 14:55:02 +0530

    remove: stopped tracking yoga-backend in git

--- yoga-backend/demote_user.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./yoga_db.sqlite');
-db.run("UPDATE users SET role = 'user' WHERE email = 'samradhmalli7@gmail.com'", (err) => {
-    if (err) console.error(err);
-    else console.log('Updated samradhmalli7@gmail.com to user role');
-    db.close();
-});
